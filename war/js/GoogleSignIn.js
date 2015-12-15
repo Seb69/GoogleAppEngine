aa42df0b0570cf03fba146b2f64e0464bce26fd9
ANDRE <sebastien.andre.seb@gmail.com>
2015-12-15 10:48:01 +0100

    commi

--- war/js/GoogleSignIn.js
+++ war/js/GoogleSignIn.js
@@ -5,7 +5,10 @@ function onSignIn(googleUser) {
     	  console.log('Name: ' + profile.getName());
     	  console.log('Image URL: ' + profile.getImageUrl());
     	  console.log('Email: ' + profile.getEmail());
-    	  document.getElementById("ID").innerHTML =profile.getId();
+    	  //document.getElementById("ID").innerHTML =profile.getId();
+//    	  document.getElementById("Name").innerHTML =profile.getName();
+//    	  document.getElementById("Image").innerHTML =profile.getImageUrl();
+//    	  document.getElementById("Email").innerHTML =profile.getEmail();
     	}
 
 
